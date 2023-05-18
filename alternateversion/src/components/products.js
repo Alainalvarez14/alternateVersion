5239579d2ed0e104e32c58984a6e12ecfc521608
Alain Alvarez <alainalvarez14@gmail.com>
2023-05-18 18:53:53 -0400

    updated styling

--- alternateversion/src/components/products.js
+++ alternateversion/src/components/products.js
@@ -4,7 +4,7 @@ import './products.css'
 
 const Products = () => {
 
-    const foodArray = JSONData.filter(product => product.Type === "Food");
+    const foodArray = JSONData.filter(product => product.Type === "Food")
     const drinkArray = JSONData.filter(product => product.Type === "Drink");
 
     const convertDate = (product) => {
@@ -19,45 +19,52 @@ const Products = () => {
 
     return (
         <div>
-
-            <div class="display-5">Food:</div>
-            {foodArray.length && (
-                <div className="productCardContainer">
-                    {foodArray.map(food => {
-                        return <div>
-                            <div class="card productCard" style={{ opacity: new Date(food.Expiration) < new Date() ? '0.4' : '' }}>
-                                <img src={`${food.Image}`} class="card-img-top productImage" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">{food.Name}</h5>
-                                    <p class="card-text">{food.Category}</p>
-                                    <p class="card-text">{food.Description}</p>
-                                    <p class="card-text">{convertDate(food)}</p>
+            <div class="page-title">
+                <h1 class="title"><strong>Bon Appétit</strong></h1>
+            </div>
+            <div class="product-container">
+                <div class="specific-product-section food-container">
+                    <h1 class="product-header">FOOD</h1>
+                    {foodArray.length && (
+                        <div className="productCardContainer">
+                            {foodArray.map(food => {
+                                return <div>
+                                    <div class="card productCard" style={{ opacity: new Date(food.Expiration) < new Date() ? '0.4' : '', pointerEvents: new Date(food.Expiration) < new Date() ? 'none' : 'cursor' }}>
+                                        <img src={`${food.Image}`} class="card-img-top productImage" alt="..." />
+                                        <div class="card-body">
+                                            <h4 class="card-title">{food.Name}</h4>
+                                            <p class="card-text"><strong>Category: </strong>{food.Category}</p>
+                                            <p class="card-text"><strong>Description: </strong>{food.Description}</p>
+                                            <p class="card-text"><strong>Expiration Date: </strong>{convertDate(food)}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            })}
                         </div>
-                    })}
+                    )}
                 </div>
-            )}
-
-            <div class="display-5 drinkHeader">Drink:</div>
-            {drinkArray.length && (
-                <div className="productCardContainer">
-                    {drinkArray.map(drink => {
-                        return <div>
-                            <div class="card productCard" style={{ opacity: new Date(drink.Expiration) < new Date() ? '0.4' : '' }}>
-                                <img src={`${drink.Image}`} class="card-img-top productImage" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">{drink.Name}</h5>
-                                    <p class="card-text">{drink.Category}</p>
-                                    <p class="card-text">{drink.Description}</p>
-                                    <p class="card-text">{convertDate(drink)}</p>
+                <div class="specific-product-section">
+                    <h1 class="product-header">DRINK</h1>
+                    {drinkArray.length && (
+                        <div className="productCardContainer">
+                            {drinkArray.map(drink => {
+                                return <div>
+                                    <div class="card productCard" style={{ opacity: new Date(drink.Expiration) < new Date() ? '0.4' : '', pointerEvents: new Date(drink.Expiration) < new Date() ? 'none' : 'cursor' }}>
+                                        <img src={`${drink.Image}`} class="card-img-top productImage" alt="..." />
+                                        <div class="card-body">
+                                            <h4 class="card-title">{drink.Name}</h4>
+                                            <p class="card-text"><strong>Category: </strong>{drink.Category}</p>
+                                            <p class="card-text"><strong>Description: </strong>{drink.Description}</p>
+                                            <p class="card-text"><strong>Expiration Date: </strong>{convertDate(drink)}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            })}
                         </div>
-                    })}
+                    )}
                 </div>
-            )}
 
+            </div >
         </div>
     )
 }
