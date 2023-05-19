c212151426e50160e17b4d3cc0b0a0c346fdd32b
Alain Alvarez <alainalvarez14@gmail.com>
2023-05-18 20:34:50 -0400

    adjusted styling and added toggle to hide and show expired products

--- alternateversion/src/components/products.js
+++ alternateversion/src/components/products.js
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import JSONData from './data.json'
 import './products.css'
 
 const Products = () => {
 
-    const foodArray = JSONData.filter(product => product.Type === "Food")
-    const drinkArray = JSONData.filter(product => product.Type === "Drink");
+    const [hideExpired, setHideExpired] = useState(false);
+    const [foodArray, setFoodArray] = useState([])
+    const [drinkArray, setDrinkArray] = useState([])
+    const allProducts = JSONData;
+
+    useEffect(() => {
+        setFoodArray(allProducts.filter(product => product.Type === "Food"));
+        setDrinkArray(allProducts.filter(product => product.Type === "Drink"));
+    }, []);
+
+    useEffect(() => {
+        if (hideExpired) {
+            setFoodArray(foodArray.filter(product => new Date(product.Expiration) >= new Date()));
+            setDrinkArray(drinkArray.filter(product => new Date(product.Expiration) >= new Date()));
+        } else {
+            setFoodArray(allProducts.filter(product => product.Type === "Food"));
+            setDrinkArray(allProducts.filter(product => product.Type === "Drink"));
+        }
+    }, [hideExpired]);
 
     const convertDate = (product) => {
         const date = new Date(product.Expiration);
@@ -18,11 +35,15 @@ const Products = () => {
     }
 
     return (
-        <div>
+        <div class="main-container">
             <div class="page-title">
                 <h1 class="title"><strong>Bon Appétit</strong></h1>
             </div>
             <div class="product-container">
+                <div class="form-check form-switch form-check-reverse expired-toggle">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckReverse" onClick={(e) => setHideExpired(e.target.checked)} />
+                    <label class="form-check-label" for="flexSwitchCheckReverse">Hide Expired Products</label>
+                </div>
                 <div class="specific-product-section food-container">
                     <h1 class="product-header">FOOD</h1>
                     {foodArray.length && (
@@ -33,7 +54,7 @@ const Products = () => {
                                         <img src={`${food.Image}`} class="card-img-top productImage" alt="..." />
                                         <div class="card-body">
                                             <h4 class="card-title">{food.Name}</h4>
-                                            <p class="card-text"><strong>Category: </strong>{food.Category}</p>
+                                            <p class="card-text"><strong>Category: </strong>{Object.values(food.Category).join(', ')}</p>
                                             <p class="card-text"><strong>Description: </strong>{food.Description}</p>
                                             <p class="card-text"><strong>Expiration Date: </strong>{convertDate(food)}</p>
                                         </div>
@@ -44,7 +65,7 @@ const Products = () => {
                     )}
                 </div>
                 <div class="specific-product-section">
-                    <h1 class="product-header">DRINK</h1>
+                    <h1 class="product-header">DRINKS</h1>
                     {drinkArray.length && (
                         <div className="productCardContainer">
                             {drinkArray.map(drink => {
@@ -53,7 +74,7 @@ const Products = () => {
                                         <img src={`${drink.Image}`} class="card-img-top productImage" alt="..." />
                                         <div class="card-body">
                                             <h4 class="card-title">{drink.Name}</h4>
-                                            <p class="card-text"><strong>Category: </strong>{drink.Category}</p>
+                                            <p class="card-text"><strong>Category: </strong>{Object.values(drink.Category).join(', ')}</p>
                                             <p class="card-text"><strong>Description: </strong>{drink.Description}</p>
                                             <p class="card-text"><strong>Expiration Date: </strong>{convertDate(drink)}</p>
                                         </div>
